1a772a4abfcf02b77c057a191fad0085c800bdcd
DamagedUnicorn <tpkzip@hotmail.com>
2023-12-28 15:00:14 +0100

    initial work

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+
+# Read character
+character = input("Character: ")
+if (character == "gendelf"): # if using python >= 3.10 this could be refactored to a match statement
+    pass
+else:
+    print("Invalid charater")
+    exit(0)
+
+# Run program
+## implement exit
+while 1:
+    inputString = input("Roll: ")
